eb60f877240197232a4f3508a69cca4fdf45f5d2
Jacek Szydlowski <szydlowski.jk@gmail.com>
2019-06-11 20:12:20 +0200

    Fixed missing this in Level.draw

--- 201906_superjumpingninja/sjn_level.js
+++ 201906_superjumpingninja/sjn_level.js
@@ -9,12 +9,15 @@ class Level {
 
         this.generated = false
 
+        this.ox = 0
+        this.oy = 0
+
         this.player = {}
         this.player.pos = new Vector(5.5, 5.5)
         this.player.spd = new Vector(0, 0.0)
     }
 
-    generate() {
+    generate () {
         let rng = new Random(this.seed)
         this.sizex = 14 // TODO randomize this
         this.sizey = 14 // TODO randomize this
@@ -34,58 +37,56 @@ class Level {
         //        debugger
     }
 
-    update() {
+    update () {
         this.player.pos.add(this.player.spd)
     }
 
-    draw() {
-        let ox = ((ww * 0.5) - (this.player.pos.x * ts)) || 0
-        let oy = ((wh * 0.5) - (this.player.pos.y * ts)) || 0
+    drawTile (x, y) {
+        ctx.strokeStyle = "#ffffff40"
+        ctx.strokeRect(Math.floor((x * ts) + this.ox), Math.floor((y * ts) + this.oy), ts, ts)
+        ctx.fillStyle = "#00ffff60"
+        if (x >= 0 && x < this.sizex && y >= 0 && y < this.sizey) {
+            switch (this.tiles[x][y]) {
+                case 0: {
+                    ctx.fillStyle = "#000000"
+                    break
+                }
+                case 1: {
+                    ctx.fillStyle = "#ffffff60"
+                    break
+                }
+                case 2: {
+                    ctx.fillStyle = "#00ff0060"
+                    break
+                }
+                case 3: {
+                    ctx.fillStyle = "#0000ff60"
+                    break
+                }
+                default: {
+                    ctx.fillStyle = "#ff00ff60"
+                }
+
+            }
+        }
+        ctx.fillRect((x * ts) + this.ox, (y * ts) + this.oy, ts, ts)
+    }
+
+    draw () {
+        this.ox = ((ww * 0.5) - (this.player.pos.x * ts)) || 0
+        this.oy = ((wh * 0.5) - (this.player.pos.y * ts)) || 0
 
         for (let x = 0; x < lvl.sizex; x++) {
             for (let y = 0; y < lvl.sizey; y++) {
-                // let tx = (x - this.player.x + (TILES_PER_VIEW/2)) * ts
-                // let ty = (y - this.player.y + (TILES_PER_VIEW/2)) * ts
-                ctx.strokeStyle = "#ffffff40"
-                ctx.strokeRect(Math.floor((x * ts)+ox), Math.floor((y * ts)+oy), ts, ts)
-                // ctx.strokeStyle = "#ffff0060"
-                // ctx.strokeRect(tx, ty, ts, ts)
-                if (lvl) {
-                    ctx.fillStyle = "#00ffff60"
-                    if (x >= 0 && x < lvl.sizex && y >= 0 && y < lvl.sizey) {
-                        switch (lvl.tiles[x][y]) {
-                            case 0: {
-                                ctx.fillStyle = "#000000"
-                                break
-                            }
-                            case 1: {
-                                ctx.fillStyle = "#ffffff60"
-                                break
-                            }
-                            case 2: {
-                                ctx.fillStyle = "#00ff0060"
-                                break
-                            }
-                            case 3: {
-                                ctx.fillStyle = "#0000ff60"
-                                break
-                            }
-                            default: {
-                                ctx.fillStyle = "#ff00ff60"
-                            }
-
-                        }
-                    }
-                    ctx.fillRect((x * ts) + ox, (y * ts) + oy, ts, ts)
-                }
+                this.drawTile(x, y)
             }
         }
 
         //! Player Draw
         ctx.fillStyle = "#884411"
         ctx.fillRect(
-            (this.player.pos.x * ts) - (ts * PLAYER_HALF_SIZE) + ox,
-            (this.player.pos.y * ts) - (ts * PLAYER_HALF_SIZE) + oy,
+            (this.player.pos.x * ts) - (ts * PLAYER_HALF_SIZE) + this.ox,
+            (this.player.pos.y * ts) - (ts * PLAYER_HALF_SIZE) + this.oy,
             ts * PLAYER_SIZE,
             ts * PLAYER_SIZE
         )
